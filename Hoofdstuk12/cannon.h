a5e0a534ecfae74ed9ba6e1c75d00d6e4466ac09
Corné <corne-projects@jammo.nl>
2022-07-05 19:10:43 +0200

    Initial commit of old project for archive

---
+++ Hoofdstuk12/cannon.h
@@ -0,0 +1,50 @@
+#ifndef CANNON_H
+#define CANNON_H
+
+class QTimer;
+
+#include <qwidget.h>
+
+class CannonField : public QWidget
+{
+	Q_OBJECT
+public:
+	CannonField(QWidget *parent=0, const char *name=0);
+
+	QSizePolicy sizePolicy() const;
+
+	int angle() const { return ang; }
+	int force() const { return f; }
+public slots:
+	void setAngle(int degrees);
+	void setForce(int newton);
+	void shoot();
+	void newTarget();
+private slots:
+	void moveShot();
+signals:
+	void hit();
+	void missed();
+	void angleChanged(int);
+	void forceChanged(int);
+protected:
+	void paintEvent(QPaintEvent*);
+private:
+	void paintShot(QPainter*);
+	void paintTarget(QPainter*);
+	void paintCannon(QPainter*);
+	QRect cannonRect() const;
+	QRect shotRect() const;
+	QRect targetRect() const;
+
+	int ang;
+	int f;
+
+	int timerCount;
+	QTimer *autoShootTimer;
+	float shoot_ang;
+	float shoot_f;
+
+	QPoint target;
+};
+#endif
